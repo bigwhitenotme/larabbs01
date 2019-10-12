2466b481ed0f6477ad112cd66e831bae6aee290a
bigwhitenotme <1664667015@qq.com>
2019-10-12 04:51:51 +0000

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-12 09:30:09
+ * @LastEditTime: 2019-10-12 09:30:09
+ * @LastEditors: your name
+ */
 const mix = require('laravel-mix');
 
 /*
@@ -12,4 +19,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
